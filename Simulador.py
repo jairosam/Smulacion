675ba87820adc5b1e7aeae92727d21593d153756
jairosam <33180728+jairosam@users.noreply.github.com>
2020-08-16 14:48:53 -0500

    Add files via upload

--- Simulador.py
+++ Simulador.py
@@ -33,21 +33,44 @@ pr_pnc = pnc / total #probabilidad de producto no conforme
 pr_desecho = desecho / pnc #probabilidad de producto desecho
 pr_reproceso = reproceso / pnc #probabilidad de producto para reproceso
 
-oper1 = pd.DataFrame()
-aleatorio = generar_aleatorios() #clasifica los productos en conformes y no conformes
-clas_producto = [] #almacena la clasificación de los productos 
+replicas = 5
 
-for probabilidad in aleatorio:
-    if probabilidad > pr_pc:
-        clas_producto.append("PNC")
-    else:
-        clas_producto.append("PC")
+for i in range(1,replicas+1):
 
-oper1["prProducto"] = aleatorio
-oper1["clasProducto"] = clas_producto
-
-aleatorio = generar_aleatorios()
-oper1["tiempoAleatorio"] = aleatorio
-oper1["tiempo"] = minimo+(maximo-minimo)*oper1.tiempoAleatorio
-oper1["costo1"] = oper1.tiempo * costo
-
+    oper1 = pd.DataFrame()
+    aleatorio = generar_aleatorios() #clasifica los productos en conformes y no conformes
+    clas_producto = [] #almacena la clasificación de los productos 
+    
+    for probabilidad in aleatorio:
+        if probabilidad > pr_pc:
+            clas_producto.append("PNC")
+        else:
+            clas_producto.append("PC")
+    
+    oper1["prProducto"] = aleatorio
+    oper1["clasProducto"] = clas_producto
+    
+    aleatorio = generar_aleatorios()
+    oper1["tiempoAleatorio"] = aleatorio
+    oper1["tiempo"] = minimo+(maximo-minimo)*oper1.tiempoAleatorio
+    oper1["costo1"] = oper1.tiempo * costo
+    
+    aleatorio = generar_aleatorios()
+    
+    oper1["aleatorioPNC"] = aleatorio
+    clas_pnc = []
+    contador = 0
+    
+    for probabilidad in oper1.aleatorioPNC:
+        if oper1.clasProducto[contador] == "PNC":
+            if probabilidad > pr_desecho:
+                clas_pnc.append("Reproceso")
+            else:
+                clas_pnc.append("Desecho")
+        else:
+            clas_pnc.append("")
+        contador += 1
+    
+    oper1["clasPNC"] = clas_pnc
+    
+    oper1.to_csv("replica " + str(i))
